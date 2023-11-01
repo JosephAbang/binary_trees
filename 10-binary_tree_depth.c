be99c25f3775228c56c3c99d73d558a84ba540f4
JosephAbang <abangjosef@gmail.com>
2023-11-01 21:57:03 +0100

    Fix Betty Errors

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,7 +8,10 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t left_count = 0, right_count = 0;
+	size_t left_count, right_count;
+
+	left_count = 0;
+	right_count = 0;
 
 	if (tree == NULL)
 		return (0);
@@ -19,10 +22,11 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		left_count++;
 	}
 
-	while  (tree->parent)
+	while (tree->parent)
 	{
 		tree = tree->parent;
 		right_count++;
 	}
+
 	return (left_count > right_count ? left_count : right_count);
 }
