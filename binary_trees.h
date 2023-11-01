b0b59280e5e4b1c19caa3e72f8041602094abf3e
JosephAbang <abangjosef@gmail.com>
2023-11-01 18:21:45 +0100

    Go through a binary tree using post-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define _BINARY_TREE_H_
 
 #include <stdlib.h>
+#include <stddef.h>
 
 /* Given structs and typedefs */
 
@@ -27,8 +28,6 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-/* Print function */
-#include <stddef.h>
 void binary_tree_print(const binary_tree_t *);
 
 
@@ -40,6 +39,9 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
 
 
 #endif /* _BINARY_TREE_H_ */
