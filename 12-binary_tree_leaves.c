fe0484b87bbcc6b68b1500223d5b1e6b30af24a0
JosephAbang <abangjosef@gmail.com>
2023-11-02 03:35:48 +0100

    Fix betty errors: Remove blank spaaces

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -16,5 +16,5 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		leaf_num++;
 
-	return (leaf_num +  binary_tree_leaves(tree->left) +  binary_tree_leaves(tree->right));
+	return (leaf_num + binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
